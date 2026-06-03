6c39b1b53a9723a3491bb675aad483bcccd86d05
franco <francobustios07@gmail.com>
2026-06-02 22:39:07 -0600

    album tests

--- web/src/lib/services/album.service.spec.ts
+++ web/src/lib/services/album.service.spec.ts
@@ -1,8 +1,27 @@
 import { modalManager, toastManager, type ActionItem } from '@immich/ui';
 import { mdiLink, mdiPlus, mdiPlusBoxOutline, mdiShareVariantOutline, mdiUpload } from '@mdi/js';
 import { createAlbumAndRedirect } from '$lib/utils/album-utils';
-import { vitest } from 'vitest';
+import { beforeEach, expect, vitest } from 'vitest';
 
 describe('album service', () => {
+  describe('getAlbumActions', () => {
+    beforeEach(()=> {
 
+    });
+    it('should work', ()=>{
+      expect(true).toStrictEqual(true);
+    });
+    it('should create a Create ActionItem', ()=>{
+      expect(true).toStrictEqual(true);
+    });
+
+    it('should show Share only when owner', ()=>{
+      expect(true).toStrictEqual(true);
+    });
+
+    it('should show Delete only when owner', ()=>{
+      expect(true).toStrictEqual(true);
+    });
+
+  });
 });
