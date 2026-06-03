0ba2db6df2f4842d6281f4a04d28535494fa0266
franco <francobustios07@gmail.com>
2026-06-02 20:08:08 -0600

    album tests

---
+++ web/src/lib/services/album.service.spec.ts
@@ -0,0 +1,8 @@
+import { modalManager, toastManager, type ActionItem } from '@immich/ui';
+import { mdiLink, mdiPlus, mdiPlusBoxOutline, mdiShareVariantOutline, mdiUpload } from '@mdi/js';
+import { createAlbumAndRedirect } from '$lib/utils/album-utils';
+import { vitest } from 'vitest';
+
+describe('album service', () => {
+
+});
